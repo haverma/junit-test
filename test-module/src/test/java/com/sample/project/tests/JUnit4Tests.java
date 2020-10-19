bc6c5d8af249ccb3235ae3472408037d4a109bb5
Alexander Kriegisch <Alexander@Kriegisch.name>
2020-10-19 12:46:07 +0700

    Reformat and re-arrange POMs and JUnit test
    I just want a clean base line before changing stuff.

--- test-module/src/test/java/com/sample/project/tests/JUnit4Tests.java
+++ test-module/src/test/java/com/sample/project/tests/JUnit4Tests.java
@@ -5,28 +5,28 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class JUnit4Tests {
-    @Test
-    public void testFilterPrimaryStack() {
-        assertTrue(true);
-    }
+  @Test
+  public void testFilterPrimaryStack() {
+    assertTrue(true);
+  }
 
-    @Test
-    public void testFilterStack0() {
-        assertTrue(true);
-    }
+  @Test
+  public void testFilterStack0() {
+    assertTrue(true);
+  }
 
-    @Test
-    public void testFilterStack1() {
-        assertTrue(true);
-    }
+  @Test
+  public void testFilterStack1() {
+    assertTrue(true);
+  }
 
-    @Test
-    public void testFilterInvalidStackKey() {
-        assertTrue(true);
-    }
+  @Test
+  public void testFilterInvalidStackKey() {
+    assertTrue(true);
+  }
 
-    @Test
-    public void testFilterProdStack() {
-        assertTrue(true);
-    }
+  @Test
+  public void testFilterProdStack() {
+    assertTrue(true);
+  }
 }
